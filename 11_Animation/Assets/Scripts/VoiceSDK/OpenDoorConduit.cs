a6321a6ad8492dcfbd2f87ed15016fabda65375d
tststs <contact@tobias-schubert.com>
2023-12-17 18:06:17 +0100

    add article 11

---
+++ 11_Animation/Assets/Scripts/VoiceSDK/OpenDoorConduit.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using Meta.WitAi;
+using UnityEngine;
+
+namespace Taikonauten.Unity.ArticleSeries
+{
+    public class OpenDoorConduit : MonoBehaviour
+    {
+        [SerializeField] private MRArticleSeriesController mRArticleSeriesController;
+        private const string OPEN_DOOR_INTENT = "open_door";
+
+        [MatchIntent(OPEN_DOOR_INTENT)]
+        public void OpenDoor(string[] values)
+        {
+            Debug.Log("OpenDoorConduit -> OpenDoor()");
+
+            string action = values[0];
+            string entity = values[1];
+
+            if (!string.IsNullOrEmpty(action) && !string.IsNullOrEmpty(entity))
+            {
+                if (action == "open" && entity == "door") {
+                    mRArticleSeriesController.OpenDoor();
+                }
+            }
+        }
+    }
+}
